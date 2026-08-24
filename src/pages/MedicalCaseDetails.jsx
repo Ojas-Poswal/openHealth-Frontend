f1445aa1cb5a3aa839f6c105c2c55d829534ae04
Ojas-Poswal <ojasposwal@gmail.com>
2026-08-24 18:38:50 +0530

    Added medical case details retrieval

--- src/pages/MedicalCaseDetails.jsx
+++ src/pages/MedicalCaseDetails.jsx
@@ -1,4 +1,40 @@
+import {useState,useEffect} from "react"
+import {useParams} from "react-router-dom"
+import api from "../api/axios"
+
 function MedicalCaseDetails(){
-    return <h1>Medical Case Details</h1>
+   const {caseId} = useParams();
+   const [medicalCase,setMedicalCase]=useState(null)
+   const fetchMedicalCase = async () => {
+      try{
+          const token = localStorage.getItem("token")
+          
+          const response = await api.get(
+              `/medical-case/${caseId}`,
+              {
+                  headers : {
+                      Authorization: `Bearer ${token}`
+                  }
+              }
+          )
+          setMedicalCase(response.data.medicalCase)
+
+    }catch(error){
+        console.error(error)
+    }
 }
+   useEffect(()=>{
+            fetchMedicalCase()
+    },[])
+   if(!medicalCase){
+    return <h1>Loading...</h1>
+   }
+   return (
+     <>
+       <h1>{medicalCase.diagnosis}</h1>
+       <p>{medicalCase.verdict}</p>
+     </>
+   )
+}
+
 export default MedicalCaseDetails
